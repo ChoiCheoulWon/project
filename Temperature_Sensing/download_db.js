1d5fc31b6be939a4a959882e9c359615c13c94a3
ChoiCheoulWon <ChoiCheoulWon@gmail.com>
2019-03-31 20:26:12 +0900

    update
    github url, 시간 전달 수정

--- Temperature_Sensing/download_db.js
+++ Temperature_Sensing/download_db.js
@@ -27,22 +27,32 @@ app.get("/data", function(req, res) {
     }
 
     console.log("Got "+ rows.length +" records");
+    console.log("type = " + rows[0].time);
+
     var html = "['Time', 'Temperature']"
-    for (var i=0; i< rows.length; i++) {
+    var i = 0;
+    for (i = 0; i< rows.length; i++) {
        html += ",['"+ rows[i].time +"', "+ rows[i].value +"]";
 	   // html += JSON.stringify(rows[i]);
     }
-
+    var last = i;
+    console.log(i);  
     fs.readFile("graph.html", 'utf8', function(err, data) {
     if (err) console.log("file error"+err);
     data = data.replace("<%DATA%>", html);
+    var time = "측정 시작 시간 = " + rows[0].time + "<br>";
+	time += "측정 종료시간 = " + rows[last-1].time + "<br>";
+    data = data.replace("<%TIME%>",time); 
+    var nodejsgiturl = "https://github.com/ChoiCheoulWon/project/blob/master/Temperature_Sensing/download_db.js";
+    var arduinogiturl = "https://github.com/ChoiCheoulWon/project/blob/master/Temperature_Sensing/Temperature_Sensing.ino";
+    data = data.replace("<%nodejsurl%>",nodejsgiturl);
+    data = data.replace("<%arduinourl%>",arduinogiturl);
     res.send(data);
-
     });
-  });
+    
+  });	
   
 })
-
 
 var server = app.listen(9000, function () {
   var host = server.address().address
